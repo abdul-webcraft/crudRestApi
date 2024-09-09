8734f3ec00096cda133edc6c7dd88acac97e63e2
abdul-webcraft <abdul@webcraftsolutions.in>
2024-09-09 17:15:19 +0530

    add some properety

--- springrestapi/src/main/java/com/restAPI/model/User.java
+++ springrestapi/src/main/java/com/restAPI/model/User.java
@@ -21,13 +21,16 @@ public class User {
     private int id;
 
     @NotBlank(message = "Name is required!")
+    @Size(min = 3,message = "Password must be atLeast 3 character")
     private String name;
     @NotBlank(message = "Email is required")
+    @Size(min = 15,message = "Password must be atLeast 15 character")
     private String email;
     @Size(min = 5,message = "Password must be atLeast 5 character")
     @NotBlank(message = "Password is required")
     private String password;
     @NotBlank(message = "Address is required")
+    @Size(min = 5,message = "Password must be atLeast 5 character")
     private String address;
 
 }
